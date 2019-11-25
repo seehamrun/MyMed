aabab701519b5c1ba987ba098a0dfd1c75256849
seehamrun <sihamh@google.com>
2019-11-24 19:50:25 -0600

    add more known medicines to the list of medicines they can use int he form

--- knownMedicine.js
+++ knownMedicine.js
@@ -1,4 +1,4 @@
-knowMedicines = {
+knownMedicines = {
   "Amoxicillin" : {
     "what": "This medication is a penicillin type antibiotic",
 	  "treats": "bacterial infections",
@@ -21,10 +21,12 @@ knowMedicines = {
     	"Difficulty with speaking", 
     	"drooling", 
     	"loss of balance control",
-		"muscle trembling, jerking, or stiffness",
-		"restlessness"
-	]
+		  "muscle trembling, jerking, or stiffness",
+		  "restlessness"
+	  ],
+    "notif": "off"
   },
+
   // https://www.drugs.com/cons/akineton.html
   "Akineton" : {
   	"what": "An oral medication",
@@ -36,6 +38,125 @@ knowMedicines = {
   	  "choreic movements", 
   	  "reduced REM sleep", 
   	  "anticholinergic syndrome"
-  	]
+  	],
+    "notif": "off"
+  },
+
+  // https://www.drugs.com/cons/bethanechol.html
+  "Bethanechol" : {
+    "what": "An oral mediciation",
+    "treats": " used to treat urinary retention (difficulty urinating), which may occur after surgery, after delivering a baby, and in other situations", 
+    "how": "Bethanechol stimulates your bladder to empty.", 
+    "usage": "Take bethanechol exactly as directed by your doctor. If you do not understand these directions, ask your pharmacist, nurse, or doctor to explain them to you.",
+    "sideEffects" : [
+      "Belching",
+      "blurred vision",
+      "diarrhea",
+      "dizziness",
+      "fainting", 
+      "frequent urination",
+      "headache",
+      "nausea",
+      "sleeplessness",
+      "stomach pain"
+    ],
+    "notif": "off"
+  },
+
+  // https://www.drugs.com/bystolic.html
+  "Bystolic": {
+    "what": "A type of beta-blocker",
+    "treats": "used to treat hypertension (high blood pressure)",
+    "how": "Beta-blockers affect the heart and circulation (blood flow through arteries and veins).", 
+    "usage": "Take Bystolic exactly as prescribed by your doctor. " +
+             "Do not take in larger or smaller amounts or for longer than recommended." +
+             "Follow the directions on your prescription label." +
+             "Take Bystolic at the same time every day, with or without food.",
+    "sideEffects" : [
+      "shortness of breath",
+      "swelling of your ankles",
+      "headache",
+      "nausea",
+      "diarrhea",
+      "insomnia"
+    ],
+    "notif": "off"
+  }, 
+
+  // https://www.drugs.com/clarinex.html
+  "Clarinex": {
+    "what": "an antihistamine",
+    "treats": "Clarinex is used to treat the symptoms of allergies, such as sneezing, watery eyes, itching, and runny nose.",
+    "how": "reduces the effects of the natural chemical histamine in the body",
+    "usage": "Take Clarinex exactly as prescribed by your doctor. " +
+             "Do not take in larger or smaller amounts or for longer than recommended." +
+             "Follow the directions on your prescription label." +
+             "Take Clarinex with or without food.",
+    "sideEffects" : [
+      "dry mouth",
+      "sore throat",
+      "muscle pain", 
+      "drowsiness",
+      "menstrual pain"
+    ],
+    "notif": "off"
+  }, 
+
+  // https://www.drugs.com/pro/cloderm.html
+  "Cloderm": {
+    "what": "a type of steroid", 
+    "treats": "used to treat inflammation and itching caused by psoriasis, eczema, or skin conditions that respond to steroid medication.",
+    "how": "helps reduce inflammation in the body", 
+    "usage": "Apply a thin layer of medicine to the affected skin and rub it in gently. Do not apply this medicine over a large area of skin unless your doctor has told you to.",
+    "sideEffects": [
+      "worsening of your condition", 
+      "redness, warmth, swelling, oozing, or severe irritation of any treated skin",
+      "high blood sugar", 
+
+    ],
+    "notif": "off"
+  },
+
+  // https://www.drugs.com/ibuprofen.html
+  "Ibuprofen": {
+    "what": "a nonsteroidal anti-inflammatory drug (NSAID",
+    "treats": "used to reduce fever and treat pain or inflammation caused by many conditions such as headache, toothache, back pain, arthritis, menstrual cramps, or minor injury.",
+    "how": "works by reducing hormones that cause inflammation and pain in the body.",
+    "usage": "",
+    "sideEffects": [
+      "changes of vision", 
+      "hearburt",
+      "shortness of breath",
+      "liver problems",
+      "kidney stones",
+      "upset stomach",
+      "bloating",
+      "dizziness",
+    ],
+    "notif": "off"
+  },
+
+  // https://www.drugs.com/insulin.html
+  "Insulin": {
+    "what": "an injected hormone",
+    "treats": "used to improve blood sugar control in adults and children with diabetes mellitus",
+    "how": "works by lowering levels of glucose (sugar) in the blood",
+    "usage": "Regular insulin is injected under the skin. A healthcare provider may teach you how to properly use the medication by yourself. Regular insulin must not be given with a pump. Do not inject this medicine into a vein or a muscle.",
+    "sideEffects": [
+      "low blood sugar",
+      "swelling",
+      "weight gain"
+    ],
+    "notif": "off"
+  },
+
+  // https://www.drugs.com/metformin.html
+  "Metformin": {
+    "what": "an oral diabetes medicine", 
+    "treats": "Type 2 diabetes",
+    "how": "used together with diet and exercise to improve blood sugar control in adults with type 2 diabetes", 
+    "usage": "",
+    "sideEffects": [],
+    "notif": "off"
   }
 }
